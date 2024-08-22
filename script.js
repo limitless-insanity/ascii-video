4d6ac504f40c4208b7ba2aefc2d90fab85ca3e75
Rohit Negi <95402933+limitless-insanity@users.noreply.github.com>
2024-08-22 13:43:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const density =
+  '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"`.                                                                   ';
+
+let video;
+let asciiDiv;
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(256, 160);
+  asciiDiv = createDiv();
+}
+function draw() {
+  video.loadPixels();
+  let asciiImage = "";
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      let pixelIndex = (i + j * video.width) * 4;
+
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, len, 0));
+      const c = density.charAt(charIndex);
+      if (c == " ") asciiImage += "&nbsp;";
+      else asciiImage += c;
+    }
+    asciiImage += "<br/>";
+  }
+  asciiDiv.html(asciiImage);
+}
